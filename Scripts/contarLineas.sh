f4f447f87f91164a90cd5b13e5272cb8ab7895c5
Hechiso <NigromanteOficialoutlook.com>
2023-10-15 17:44:06 -0600

    Se agrega script [sirve para contar las lineas de un archivo .txt]

---
+++ Scripts/contarLineas.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Nombre del archivo
+file_name="instalar.sh"
+
+# Verifica si el archivo existe
+if [ ! -f "$file_name" ]; then
+  echo "El archivo $file_name no existe."
+  exit 1
+fi
+
+# Cuenta las líneas del archivo y muestra el resultado
+line_count=$(wc -l < "$file_name")
+echo "El archivo $file_name tiene $line_count líneas."
+
